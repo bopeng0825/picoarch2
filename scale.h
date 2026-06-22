fbe1dc1d2e29639eb0b632917f16637b26ef415c
pb <bobo-peng@qq.com>
2026-06-22 09:44:36 +0800

    Update scale.h

--- scale.h
+++ scale.h
@@ -3,13 +3,8 @@
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
-
-#define HUD_LEN 31
-#else
 #define HUD_LEN 41
-#endif
 
-#define SCREEN_HEIGHT 240
 #define SCREEN_BPP 2
 #define SCREEN_PITCH (SCREEN_BPP * SCREEN_WIDTH)
 
